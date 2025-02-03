8474df55f97e26c2a24474cfeea75dc9b9ceeda1
arshiagarg2006 <arshiagarg2006@gmail.com>
2025-02-02 23:37:36 -0500

    Creating Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 import './App.css';
 
 function App() {
