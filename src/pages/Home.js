e3e77cedbec3e87a0d247ddfad1230f4e704c0ff
arshiagarg2006 <arshiagarg2006@gmail.com>
2025-03-03 14:44:49 -0500

    layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ function Home() {
             adrenaline and excitement, like mountain biking, snowboarding, and 
             surfing. My goal in life is to visit every national park. I'm also 
             a thriller and sci-fi movie enthusiast and would love any recs. 
-            Connect with me on <a href="https://www.linkedin.com/in/arshia--garg/">LinkedIn</a>.
+            \nConnect with me on <a href="https://www.linkedin.com/in/arshia--garg/">LinkedIn</a>.
             </p>
           </div>
         </div>
