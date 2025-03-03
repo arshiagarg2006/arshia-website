5ec260266214a514fd429119172b506e092e68c9
arshiagarg2006 <arshiagarg2006@gmail.com>
2025-03-03 15:08:56 -0500

    layout

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -9,7 +9,13 @@ function Resume() {
           data="/assets/resume.pdf"
           type="application/pdf"
           width="100%"
-          height="1000px"
+          height="1200px"
+          style={{
+            maxWidth: '100%',
+            width: '1600px',
+            margin: '0 auto',
+            display: 'block'
+          }}
         >
           <p>
             It appears you don't have a PDF plugin for this browser.
