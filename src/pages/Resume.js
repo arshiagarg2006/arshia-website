5a60a0ef0a0b5c3cb3684009e293abe178eaac1c
arshiagarg2006 <arshiagarg2006@gmail.com>
2025-03-03 14:55:33 -0500

    layout

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 function Resume() {
   return (
-    <div className="resume">
+    <div className="resume-container">
       <h2>Resume</h2>
       <div className="pdf-container">
         <object
           data="/assets/resume.pdf"
           type="application/pdf"
           width="100%"
-          height="800px"
+          height="1000px"
         >
           <p>
             It appears you don't have a PDF plugin for this browser.
