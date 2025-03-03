d05686a0ac0be42f75fa2064b0583f91f1867299
arshiagarg2006 <arshiagarg2006@gmail.com>
2025-03-03 12:03:10 -0500

    outline

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Resume from "./pages/Resume";
+import CoverLetter from "./pages/CoverLetter";
+import ElevatorPitch from "./pages/ElevatorPitch";
+import SampleWork from "./pages/SampleWork";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/resume">Resume</Link></li>
+            <li><Link to="/cover-letter">Cover Letter</Link></li>
+            <li><Link to="/elevator-pitch">Elevator Pitch</Link></li>
+            <li><Link to="/sample-work">Sample Work</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/cover-letter" element={<CoverLetter />} />
+          <Route path="/elevator-pitch" element={<ElevatorPitch />} />
+          <Route path="/sample-work" element={<SampleWork />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
